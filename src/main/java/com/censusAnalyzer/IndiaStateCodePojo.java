c668dc7382f680a5252acb00171f9ff5484cebf0
shindepriti <pritishinde34@gmail.com>
2020-04-02 23:29:12 +0530

    ADDED:Load State Code Data Return NumberOFRecord And Custom Exception

---
+++ src/main/java/com/censusAnalyzer/IndiaStateCodePojo.java
@@ -0,0 +1,27 @@
+package com.censusAnalyzer;
+
+import com.opencsv.bean.CsvBindByName;
+
+public class IndiaStateCodePojo {
+    @CsvBindByName(column = "SrNo",required = true)
+    private int srNo;
+    @CsvBindByName(column = "State",required = true)
+    private String state;
+    @CsvBindByName(column = "Name",required = true)
+    private String name;
+    @CsvBindByName(column = "TIN",required = true)
+    private int tin;
+    @CsvBindByName(column = "StateCode")
+    private String stateCode;
+
+    @Override
+    public String toString() {
+        return "IndiaStateCodePojo{" +
+                "srNo=" + srNo +
+                ", state='" + state + '\'' +
+                ", name='" + name + '\'' +
+                ", tin=" + tin +
+                ", stateCode='" + stateCode + '\'' +
+                '}';
+    }
+}
